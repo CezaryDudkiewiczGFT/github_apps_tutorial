10214404cf51955a76ad8e4763f98c4c1fa57fe7
Cezary Dudkiewicz <cezary.dudkiewicz@gft.com>
2025-07-09 15:09:51 +0200

    Changes in startup.sh and added '/' post endpoint

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from starlette.requests import Request
+
+from main import main
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/")
+async def read_response(request: Request):
+    req = await request.json()
+    print(req['action'])
+
+
+main()
