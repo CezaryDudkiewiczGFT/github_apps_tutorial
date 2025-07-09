08109aa3ef0ccd43cba6bea4e8dd79ef30c423de
Cezary Dudkiewicz <cezary.dudkiewicz@gft.com>
2025-07-09 11:48:39 +0200

    Some functionality

---
+++ main.py
@@ -0,0 +1,33 @@
+# todo: create issue bot app
+import os
+from github import Github, GithubIntegration
+GITHUB_APP_ID = 1552123
+from github import Auth
+
+deployment = os.getenv("DEPLOYMENT")
+if deployment == "LOCAL":
+    from secrets import PRIVATE_KEY
+    auth = Auth.AppAuth(GITHUB_APP_ID, PRIVATE_KEY)
+elif deployment == "DEV":
+    from azure.identity import ManagedIdentityCredential
+    token = ManagedIdentityCredential().get_token().token
+    auth = Auth.AppAuthToken(token)
+elif deployment == "LOCAL_USER_TOKEN":
+    from secrets import TOKEN
+    auth = Auth.Token(TOKEN)
+    g = Github(auth=auth)
+    for repo in g.get_user().get_repos():
+        print(repo.name)
+        print(repo.create_issue(title="App created issue"))
+else:
+    raise Exception("'DEPLOYMENT' env variable must be set!")
+
+if deployment != "LOCAL_USER_TOKEN":
+    g = GithubIntegration(auth=auth)
+    inst = g.get_installations()[0]
+    for repo in inst.get_repos():
+        print(repo.name)
+        print(repo.create_issue(title=f"App created issue. {deployment}"))
+
+# To close connections after use
+g.close()
